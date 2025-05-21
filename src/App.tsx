d39f6f11176cc67e78a774cf896570bc570f2527
Arthur Barros <arthurpbarros@hotmail.com>
2025-05-21 17:39:45 -0300

    salvando state no localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import './styles/global.css'
 import './styles/theme.css'
 
-import { Home } from './pages/Home';
-import { NotFound } from './pages/NotFound';
-import { AboutPomodoro } from './pages/AboutPomodoro';
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 import { MessageContainer } from './components/MessagesContainer';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { MainRouter } from './routers/MainRouter';
 
 export function App() {
   return (
     <TaskContextProvider>
       <MessageContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <MainRouter />
       </MessageContainer>
     </TaskContextProvider>
   );
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,7 @@
-import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, Sun, SunIcon } from 'lucide-react';
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router';
+import { RouterLink } from '../RounterLink';
 
 type TemasDisponiveis = 'dark' | 'light';
 
@@ -31,17 +31,17 @@ export function Menu() {
     }, [theme]);
 
     return <nav className={styles.menu}>
-        <Link className={styles.menuLink} to='/' aria-label='Ir para a home' title='Ir para a home'>
+        <RouterLink className={styles.menuLink} href='/' aria-label='Ir para a home' title='Ir para a home'>
             <HouseIcon/>
-        </Link>
-        <Link className={styles.menuLink} to='#' aria-label='Ver histórico' title='Ver histórico'>
+        </RouterLink>
+        <RouterLink className={styles.menuLink} href='/history/' aria-label='Ver histórico' title='Ver histórico'>
             <HistoryIcon/>
-        </Link>
-        <Link className={styles.menuLink} to='#' aria-label='Configurações' title='Configurações'>
+        </RouterLink>
+        <RouterLink className={styles.menuLink} href='/settings/' aria-label='Configurações' title='Configurações'>
             <SettingsIcon/>
-        </Link>
-        <Link className={styles.menuLink} to='#' aria-label='Mudar tema' title='Mudar tema' onClick={handleThemeChange}>
+        </RouterLink>
+        <a className={styles.menuLink} href='#' aria-label='Mudar tema' title='Mudar tema' onClick={handleThemeChange}>
             {nextThemeIcon[theme]}
-        </Link>
+        </a>
     </nav>;
 }
